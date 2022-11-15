0d218584b210a23ceb89ca3bd69e5cd27ac783bb
Jozsef Geng <jozsef.geng@fondof.de>
2022-11-15 11:41:43 +0100

    Add Unit Test for: - RestDeleteCompanyUserRequestMapperTest.php - RestResponseBuilderTest.php

--- tests/FondOfSpryker/Glue/CompanyUsersRestApi/Processor/Builder/RestResponseBuilderTest.php
+++ tests/FondOfSpryker/Glue/CompanyUsersRestApi/Processor/Builder/RestResponseBuilderTest.php
@@ -4,13 +4,25 @@ namespace FondOfSpryker\Glue\CompanyUsersRestApi\Processor\Builder;
 
 use Codeception\Test\Unit;
 use FondOfSpryker\Glue\CompanyUsersRestApi\CompanyUsersRestApiConfig;
+use Generated\Shared\Transfer\CompanyUserTransfer;
 use Generated\Shared\Transfer\RestErrorMessageTransfer;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface;
+use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface;
 use Spryker\Glue\GlueApplication\Rest\JsonApi\RestResponseInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class RestResponseBuilderTest extends Unit
 {
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Generated\Shared\Transfer\CompanyUserTransfer
+     */
+    protected $companyUserTransferMock;
+
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceInterface
+     */
+    protected $restResourceMock;
+
     /**
      * @var \PHPUnit\Framework\MockObject\MockObject|\Spryker\Glue\GlueApplication\Rest\JsonApi\RestResourceBuilderInterface
      */
@@ -33,11 +45,19 @@ class RestResponseBuilderTest extends Unit
     {
         parent::_before();
 
+        $this->restResourceMock = $this->getMockBuilder(RestResourceInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->restResourceBuilderMock = $this->getMockBuilder(RestResourceBuilderInterface::class)
             ->disableOriginalConstructor()
             ->getMock();
 
         $this->restResponseMock = $this->getMockBuilder(RestResponseInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->companyUserTransferMock = $this->getMockBuilder(CompanyUserTransfer::class)
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -112,6 +132,50 @@ class RestResponseBuilderTest extends Unit
         static::assertEquals(
             $this->restResponseMock,
             $this->restResponseBuilder->buildEmptyRestResponse(),
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function testBuildRestResponse(): void
+    {
+        $this->companyUserTransferMock->expects(static::atLeastOnce())
+            ->method('toArray')
+            ->willReturn([]);
+
+        $this->companyUserTransferMock->expects(static::atLeastOnce())
+            ->method('getCompanyUserReference')
+            ->willReturn('company-user-reference');
+
+        $this->restResourceBuilderMock->expects(static::atLeastOnce())
+            ->method('createRestResource')
+            ->willReturn($this->restResourceMock);
+
+        $this->restResourceMock->expects(static::atLeastOnce())
+            ->method('setPayload')
+            ->with($this->companyUserTransferMock)
+            ->willReturn($this->restResourceMock);
+
+        $this->restResourceBuilderMock->expects(static::atLeastOnce())
+            ->method('createRestResponse')
+            ->willReturn($this->restResponseMock);
+
+        $this->restResponseMock->expects(static::atLeastOnce())
+            ->method('addResource')
+            ->with($this->restResourceMock)
+            ->willReturn($this->restResponseMock);
+
+        $restResponse = $this->restResponseBuilder->buildRestResponse($this->companyUserTransferMock);
+
+        static::assertInstanceOf(
+            RestResponseInterface::class,
+            $restResponse,
+        );
+
+        static::assertEquals(
+            $this->restResponseMock,
+            $restResponse,
         );
     }
 }
